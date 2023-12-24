1c5b5fb60a3ce827cc8c618bc3c25fb5cc98ea79
vladz <vlad.zasko@gmail.com>
2023-12-24 21:43:17 +0100

    test

--- src/features/blogs/repositories/blog-db-repository.ts
+++ src/features/blogs/repositories/blog-db-repository.ts
@@ -24,7 +24,7 @@ export class BlogMemoryDbRepository {
         return blogMapper(blog)
     }
 
-    static async createBlog(createData : CreateBlogModel):Promise<string> {
+    static async createBlog(createData : CreateBlogModel):Promise<BlogsViewModel> {
         const newBlog = {
             ...createData,
             createdAt: new Date().toISOString(),
@@ -33,8 +33,10 @@ export class BlogMemoryDbRepository {
 
         const blog = await blogsCollection.insertOne(newBlog)
 
-        return blog.insertedId.toString()
-
+        return {
+            ...newBlog,
+            id: blog.insertedId.toString()
+        }
     }
     static async updateBlog(id: string , updateData:UpdateBlogModel): Promise<boolean> {
         const foundBlog = await blogsCollection.updateOne({_id:new ObjectId(id)}, {
