5325a3f8e7dc9514b683eac8a5baa0bba511b428
vladz <vlad.zasko@gmail.com>
2024-01-13 21:12:35 +0100

    resending code and validation

--- src/features/auth/validator/auth-confirmation-validator.ts
+++ src/features/auth/validator/auth-confirmation-validator.ts
@@ -4,12 +4,15 @@ import {authQueryRepository} from "../repositories/auth-query-repository";
 
 export const codeValidation = body('code').isString().trim().withMessage('Incorrect code!')
     .custom(async (value) => {
-    const user = await authQueryRepository.findUserByConfirmationCode(value)
-    if (user?.emailConfirmation?.isConfirmed) {
-        throw Error('email is already confirmed')
-    }
-    return true
-}).withMessage('email is already confirmed')
+        const user = await authQueryRepository.findUserByConfirmationCode(value)
+        if (user?.emailConfirmation?.isConfirmed) {
+            throw Error('email is already confirmed')
+        }
+        if (user?.emailConfirmation?.isConfirmed !== value) {
+            throw Error('Incorrect code')
+        }
+        return true
+    }).withMessage('email is already confirmed')
 
-export const authConfirmationValidator = () => [codeValidation , inputValidation]
+export const authConfirmationValidator = () => [codeValidation, inputValidation]
 
